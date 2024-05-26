89f18fa6319116af1d392decb3f9b781e9eaad7f
Lakshay <59942900+Lakshay-G@users.noreply.github.com>
2024-05-26 15:41:55 -0400

    Removing form for now

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -29,7 +29,7 @@ const Contact = () => {
         link="https://images.unsplash.com/photo-1534536281715-e28d76689b4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
       />
       <ContactSection />
-      <Form />
+      {/*<Form />*/}
       <Navbar />
       <Footer />
       <ScrollToTop />

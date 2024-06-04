da00a129968a6646e87137557f7131f28ba2eb4f
Lakshay <59942900+Lakshay-G@users.noreply.github.com>
2024-06-04 02:46:29 -0400

    Fixed Navbar, Tried Contact Form

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -4,8 +4,17 @@ import { getFirestore } from "firebase/firestore";
 // import dotenv from "dotenv-webpack";
 
 // import { getAnalytics } from "firebase/analytics";
-
 const firebaseApp = {
+  // apiKey: "<%= process.env.REACT_APP_FIREBASE_API_KEY %>",
+  // // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: "<%= process.env.REACT_APP_FIREBASE_AUTH_DOMAIN %>",
+  // // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // // projectId: "<%= process.env.REACT_APP_FIREBASE_PROJECT_ID%>",
+  // projectId: "<%= process.env.REACT_APP_FIREBASE_PROJECT_ID%>",
+  // storageBucket: "<%= process.env.REACT_APP_FIREBASE_STORAGE_BUCKET%>",
+  // messagingSenderId: "<%= process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID%>",
+  // appId: "<%= process.env.REACT_APP_FIREBASE_APP_ID%>",
+  // measurementId: "<%= process.env.REACT_APP_FIREBASE_MEASUREMENT_ID%>",
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
